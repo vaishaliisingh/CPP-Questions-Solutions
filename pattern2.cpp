386bfeb4b2007693d66a96571d5af96c1c00ada1
Vaishali Singh <140580678+vaishaliisingh@users.noreply.github.com>
2024-08-22 20:21:02 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int row = 1;
+    int value = 1;
+    while (row <= n){
+       int col = 1;
+       while (col <= n){
+       cout << value << " ";
+       value = value + 1;
+       col = col + 1;
+       }
+    cout << endl;
+    row = row +1;
+    }
+
+}
